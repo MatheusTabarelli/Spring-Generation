54c523ef7dfe19df05654008b7929602f3e1209f
Matheus Tabarelli <12tabarelli@gmail.com>
2021-07-23 15:42:48 -0300

    Exercícios Introdução a Spring

--- 23-07-2021/habilidades/src/main/java/br/org/generation/habilidades/controller/HabilidadesSemana.java
+++ 23-07-2021/habilidades/src/main/java/br/org/generation/habilidades/controller/HabilidadesSemana.java
@@ -12,12 +12,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//solicitando comandos para pacote da aplicação
+/**
+ * 
+ * /@RestController: Annotation (Anotação), que indica que a Classe é uma RestController,
+ * ou seja, é responsável por responder às requisições http enviadas para um endpoint
+ * (endereço) definido na anotação @RequestMapping
+ * 
+ * @RequestMapping("/postagens"): Annotation (Anotação), que indica o endpoint (endereço) 
+ * que a controladora responderá as requisições 
+ * 
+ */
+
 @RestController
 @RequestMapping("/habilidades")
 public class HabilidadesSemana {
 	
-	//criando string para printar texto 
+	/**
+	 * Exibir a mensagem
+	 *  
+	 * /@GetMapping: Annotation (Anotação), que indica que o método abaixo responderá todaas as 
+	 * requisições do tipo GET que forem enviadas no endpoint /hello
+	 * 
+	 * O Método hello() será do tipo String porque ele retornará um String (Hello Turma 25!)
+	 * 
+	 * return "Hello Turma 25!";: Retorna a mensagem
+	 *
+	 */
+	
 	@GetMapping
 	public String habil() {
 		return "Para a realização desta atividade, foram utilizadas as seguintes mentalidades e habilidades: "
