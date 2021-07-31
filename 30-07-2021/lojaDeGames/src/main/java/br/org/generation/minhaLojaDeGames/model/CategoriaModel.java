69616c9a348202d7a6309655a7bb932d8968cf70
Matheus Tabarelli <12tabarelli@gmail.com>
2021-07-30 21:47:10 -0300

    Exercício API Rest com validação no Postman

---
+++ 30-07-2021/lojaDeGames/src/main/java/br/org/generation/minhaLojaDeGames/model/CategoriaModel.java
@@ -0,0 +1,64 @@
+package br.org.generation.minhaLojaDeGames.model;
+
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//definindo tabela a ser armazenada no banco de dados
+@Entity
+@Table(name = "tb_categorias")
+
+public class CategoriaModel {
+
+	//criando atributo ID
+	@Id
+	//definindo o atributo ID como auto increment
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	//criando atributo tipo, não nulo
+	@NotNull(message = "O atributo tipo precisa ser preenchido!")
+	private String tipo;
+	
+	//criando relação da tabela categoria com a tabela produtos, através do atributo produto
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	//utilizando annotation para evitar loopings na saída do programa
+	@JsonIgnoreProperties("categoria")
+	private List<ProdutoModel> Produto;
+
+	//gerando getters e setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<ProdutoModel> getProduto() {
+		return Produto;
+	}
+
+	public void setProduto(List<ProdutoModel> produto) {
+		Produto = produto;
+	}
+
+	
+}
---
+++ 30-07-2021/lojaDeGames/src/main/java/br/org/generation/minhaLojaDeGames/model/ProdutoModel.java
@@ -0,0 +1,94 @@
+package br.org.generation.minhaLojaDeGames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//definindo tabela a ser armazenada no banco de dados
+@Entity
+@Table(name = "tb_produtos")
+
+public class ProdutoModel {
+
+	//criando atributo ID
+	@Id
+	//definindo o atributo ID como auto increment
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	//criando atributo nome, não nulo
+	@NotNull(message = "O atributo nome precisa ser preenchido!")
+	//definindo tamanho mininmo e maximo do atributo nome
+	@Size(min = 4, max = 25, message = "O atributo descrição deve ter entre 4 e 25 caracteres!")
+	private String nome;
+
+	//criando atributo descricao, não nulo
+	@NotNull(message = "O atributo descrição precisa ser preenchido!")
+	//definindo tamanho mininmo e maximo do atributo descrição
+	@Size(min = 4, max = 200, message = "O atributo descrição deve ter entre 4 e 200 caracteres!")
+	private String descricao;
+
+	//criando atributo preço, não nulo
+	@NotNull(message = "O atributo preço precisa ser preenchido!")
+	//definindo atributo preco para valores positivos
+	@Positive(message = "O atributo preço precisa ser maior do que zero!")
+	private BigDecimal preco;
+	
+	//criando relação da tabela produtos com a tabela categoria, através do atributo categoria
+	@ManyToOne
+	//utilizando annotation para evitar loopings na saída do programa
+	@JsonIgnoreProperties("produto")
+	private CategoriaModel categoria;
+
+	//gerando getters e setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public CategoriaModel getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaModel categoria) {
+		this.categoria = categoria;
+	}
+
+	
+}
